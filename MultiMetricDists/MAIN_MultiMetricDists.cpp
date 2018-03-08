43b60dec9abfbf24d62857439b26213b07fc604d
larry andrews <larry6640995@gmail.com>
2018-03-08 08:44:23 -0800

    working on tetrahedron face areas

--- MultiMetricDists/MAIN_MultiMetricDists.cpp
+++ MultiMetricDists/MAIN_MultiMetricDists.cpp
@@ -92,6 +92,7 @@ static double TriangleAreaFromSides(const double a, const double b, const double
    const double s = (a + b + c) / 2.0; // s is half the perimeter
    const double trialValue = s * (s - a)*(s - b)*(s - c); // Heron's formula
    const double sign = (trialValue >= 0.0) ? 1.0 : -1.0;
+   return sign * std::sqrt(std::abs(trialValue));
 }
 
 static double SqrtTriangleAreaFromSides(const double a, const double b, const double c)
