52396a5d763fd95a81af219673cf88ff4dc86a70
larry andrews <larry6640995@gmail.com>
2018-03-23 08:58:00 -0700

    fixing random lattice generation, particularly in triangle follower, but may affect other programs

--- SVG_TriangleFollow/TriangleFollowTools.h
+++ SVG_TriangleFollow/TriangleFollowTools.h
@@ -30,10 +30,16 @@ G6 SetCellParams(const G6& vin)
    return v;
 };
 
-static double SqrtTriangleAreaFromSides(const double a, const double b, const double c)
+static double TriangleAreaFromSides(const double a, const double b, const double c)
 {
    const double s = (a + b + c)/2.0; // s is half the perimeter
    const double trialValue = s*(s - a)*(s - b)*(s - c); // Heron's formula
+   const double sign = (trialValue >= 0.0) ? 1.0 : -1.0;
+   return sign * std::sqrt(std::abs(trialValue));
+}
+static double SqrtTriangleAreaFromSides(const double a, const double b, const double c)
+{
+   const double trialValue = TriangleAreaFromSides(a, b, c); // Heron's formula
    const double sign = (trialValue >= 0.0) ? 1.0 : -1.0;
    return sign * std::sqrt( std::abs( trialValue ) );
 }
