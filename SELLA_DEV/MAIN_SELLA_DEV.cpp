82f5fb36b61c884fc36fade2a1e44f93f84ead4b
larry andrews <larry6640995@gmail.com>
2018-08-30 08:27:33 -0700

    Change names of Delone types to modern form (like "K" -> "C")
    Sella -- correct three projectors and then the matices for the Delone types

---
+++ SELLA_DEV/MAIN_SELLA_DEV.cpp
@@ -0,0 +1,175 @@
+// SELLA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+#include "LatticeConverter.h"
+#include "LRL_ReadLatticeData.h"
+#include "LRL_StringTools.h"
+#include "S6.h"
+#include "Sella.h"
+#include "Selling.h"
+
+
+std::string Letters(void) {
+   return "V,G,D,S,P,A,B,C,I,F,R,C3,G6,S6,B4,D7";
+}
+
+void OutputCellData(LatticeConverter& converter, const std::vector<LRL_ReadLatticeData>& cellDataList) {
+   const std::string letters = Letters();
+   for (size_t i1 = 0; i1 < cellDataList.size(); ++i1) {
+      const LRL_ReadLatticeData& rcd = cellDataList[i1];
+      const std::string lattice = rcd.GetLattice();
+      size_t place = letters.find(lattice);
+      if (letters.find(LRL_StringTools::strToupper(lattice)) == std::string::npos) continue;
+      std::cout << std::endl;
+      std::cout << "LRL_Cell # " << i1 << "  *******************************" << std::endl;
+      converter.Output("Input Data", lattice, rcd.GetCell());
+      std::cout << std::endl;
+      converter.NiggliReducedOutput("Niggli Reduced", lattice, rcd.GetCell());
+      std::cout << std::endl;
+      converter.DeloneReducedOutput("Delone Reduced", lattice, rcd.GetCell());
+      std::cout << std::endl;
+      const LRL_Cell reducedCell = converter.DeloneReduceCell(lattice, rcd.GetCell());
+   }
+}
+
+std::vector<LRL_ReadLatticeData> GetInputCells(void) {
+   const std::string letters = Letters();
+   std::string lattice;
+   std::vector<LRL_ReadLatticeData> cellDataList;
+   LRL_ReadLatticeData rcd;
+   while (lattice != "EOF") {
+      rcd.read();
+      lattice = rcd.GetLattice();
+      if ((!lattice.empty()) && (letters.find(toupper(lattice[0])) != std::string::npos))
+         cellDataList.push_back(rcd);
+   }
+
+   return cellDataList;
+}
+
+std::vector<S6> GetInputSellingReducedVectors() {
+   std::vector<S6> v;
+   std::vector<LRL_ReadLatticeData> input = GetInputCells();
+   LatticeConverter converter;
+
+
+   for ( unsigned long i=0; i<input.size(); ++i ) {
+      const LRL_ReadLatticeData& rcd = input[i];
+      const S6 s6 = converter.SellingReduceCell(input[i].GetLattice(), input[i].GetCell());
+      v.push_back(s6);
+   }
+   return v;
+}
+
+int seed = 19192;
+GenerateRandomLattice<S6> generator(seed);
+
+S6 GenerateRandomSphere(const double radius) {
+   S6 s6;
+   for (unsigned long i = 0; i < 6; ++i) {
+      s6[i] = 2.0 * rhrand.urand() - 1.0;
+   }
+   return s6 / s6.norm();
+}
+
+S6 ScaleAndThenPerturbByPercent( const S6& s6, const double scale, const double percent) {
+   const S6 master = scale * s6 / s6.norm();
+   const S6 ran = GenerateRandomSphere(percent/100.0);
+   const S6 scaledRandom = scale * percent / 100.0 * ran / ran.norm();
+
+   const double n1 = master.norm();
+   const double n2 = scaledRandom.norm();
+   const double n3 = (master + scaledRandom).norm();
+   return master + scaledRandom;
+}
+
+void SellaLineTest(const S6& s6) {
+   const S6 delta = generator.randSellingReduced();
+   const unsigned long nsteps = 100;
+   S6 step = 2.0 * delta / (nsteps - 1);
+   step /= step.norm();
+   S6 stepper = s6 + step * nsteps/2;
+   Sella sella;
+   std::vector<std::vector<std::pair<std::string, double> >  > fits;
+
+   for (unsigned long i = 0; i < nsteps; ++i) {
+      S6 reducedStepper;
+      const bool b = Selling::Reduce(stepper, reducedStepper);
+      reducedStepper = s6.norm() * reducedStepper / reducedStepper.norm();
+      const std::vector<std::pair<std::string, double> > out = sella.GetVectorOfFits(reducedStepper);
+      stepper -= step;
+      fits.push_back(out);
+
+      if (i == 0) {
+         for (unsigned long k = 0; k < out.size(); ++k) {
+            std::cout << out[k].first << " ";
+         }
+      }
+      for (unsigned long k = 0; k < out.size(); ++k) {
+         std::cout << out[k].second << " ";
+      }
+      std::cout << std::endl;
+   }
+   std::cout << fits.size() << std::endl;
+}
+
+int main()
+{
+
+   //std::vector<LabeledSellaMatrices> allPerps = Sella::CreateAllPerps();
+
+
+   Sella sella;
+   sella.WriteSellaMatrices("funcName", sella.GetPerps());
+   sella.WriteSellaMatrices("funcName", sella.GetProjectors());
+   exit(0);
+
+   std::vector<S6> vLat = GetInputSellingReducedVectors();
+
+   //SellaLineTest(vLat[0]);
+   //exit(0);
+
+   const unsigned long n = 1000;
+
+   for (unsigned long lat = 0; lat < vLat.size(); ++lat) {
+      std::vector<std::pair<std::string, double> > out;
+      std::vector<double> sum(24, 0.0);
+      std::vector<double> sumsq(24, 0.0);
+      std::vector<double> maxDist(24, -DBL_MAX);
+
+      double sign = 1.0;
+      for (unsigned long k = 0; k < n; ++k) {
+         out = sella.GetVectorOfFits(ScaleAndThenPerturbByPercent(vLat[lat], 1000.0, sign * 1.0));
+         if ( out.size() < sum.size()) {
+            sum.resize(out.size());
+            sumsq.resize(out.size());
+         }
+         for (unsigned long i = 0; i < out.size(); ++i) {
+            if ( out[i].second > 2.0 && out[i].first == "C5 ") {
+               const int i19191 = 19191;
+            }
+            sum[i] += out[i].second;
+            sumsq[i] += out[i].second*out[i].second;
+            maxDist[i] = std::max(maxDist[i], out[i].second);
+         }
+         sign = -sign;
+      }
+
+      for (unsigned long i = 0; i < sum.size(); ++i) {
+         const double avg = sum[i] / n;
+         const double stddev = sqrt(sumsq[i] / n - avg * avg);
+         std::cout << out[i].first << "  " << avg << "   " << stddev << "      max " << maxDist[i];
+         if (maxDist[i] > 0.0) std::cout << "    avg/max " << avg / maxDist[i];
+         std::cout << std::endl;
+      }
+      std::cout << std::endl;
+   }
+
+   sella.ShowIndexResults();
+
+   //sella.TestPerps();
+   const int  i19191 = 19191;
+}
--- SELLA_DEV/Sella.h
+++ SELLA_DEV/Sella.h
@@ -32,38 +32,29 @@ private:
 class Sella {
 public:
 
+   //static const std::vector< std::pair<std::string, MatS6> > vDeloneTypes/* = Delone::LoadLabeledLatticeTypeProjectors();*/;
+
    Sella();
    double TestOneType(const std::string& label, const S6 &s6, const std::vector<MatS6>& vm);
+   void TestAllTypes(const S6& s6);
+   void TestPerps();
    std::vector<std::pair<std::string, double> > GetVectorOfFits(const S6& s6);
+   void ShowIndexResults() const;
+   void WriteSellaMatrices(const std::string& functinoName, const std::vector<LabeledSellaMatrices>& mat) const;
 
-   std::vector<LabeledSellaMatrices> GetPerps(void) const {
+   std::vector<LabeledSellaMatrices> GetPerps( void ) const {
       return perps;
    }
 
-   std::vector<LabeledSellaMatrices> GetProjectors(void) const {
+   std::vector<LabeledSellaMatrices> GetProjectors( void) const {
       return projectors;
    }
 
    static std::vector<LabeledSellaMatrices> CreateAllPerps();
-   static std::vector<LabeledSellaMatrices> CreateAllPrjs();
-
-   double GetBestFitForDeloneType(const std::string& type);
-   std::pair<std::string, double>  GetBestFitForCrystalSystem(const std::string& type, const S6& s6);
-   double GetFitForDeloneType(const std::string& type, const S6& s6);
-   std::vector<std::pair<std::string, double> > GetAllFitsForCrystalSystem(const std::string& type);
-
-   void GetDeloneTypesForCrystalType();
-   void GetCrystalTypesForDeloneType();
-
-private:
-   static const std::vector<LabeledSellaMatrices> projectors;
-   static const std::vector<LabeledSellaMatrices> perps;
-
-private:
    static LabeledSellaMatrices CreatePerps_H4();
-   static LabeledSellaMatrices CreatePerps_C1();
-   static LabeledSellaMatrices CreatePerps_C3();
-   static LabeledSellaMatrices CreatePerps_C5();
+   static LabeledSellaMatrices CreatePerps_K1();
+   static LabeledSellaMatrices CreatePerps_K3();
+   static LabeledSellaMatrices CreatePerps_K5();
    static LabeledSellaMatrices CreatePerps_M1A();
    static LabeledSellaMatrices CreatePerps_M1B();
    static LabeledSellaMatrices CreatePerps_M2A();
@@ -76,16 +67,17 @@ private:
    static LabeledSellaMatrices CreatePerps_O3();
    static LabeledSellaMatrices CreatePerps_O4();
    static LabeledSellaMatrices CreatePerps_O5();
-   static LabeledSellaMatrices CreatePerps_T1();
-   static LabeledSellaMatrices CreatePerps_T2();
-   static LabeledSellaMatrices CreatePerps_T5();
+   static LabeledSellaMatrices CreatePerps_Q1();
+   static LabeledSellaMatrices CreatePerps_Q2();
+   static LabeledSellaMatrices CreatePerps_Q5();
    static LabeledSellaMatrices CreatePerps_R1();
    static LabeledSellaMatrices CreatePerps_R3();
 
+   static std::vector<LabeledSellaMatrices> CreateAllPrjs();
    static LabeledSellaMatrices CreatePrjs_H4();
-   static LabeledSellaMatrices CreatePrjs_C1();
-   static LabeledSellaMatrices CreatePrjs_C3();
-   static LabeledSellaMatrices CreatePrjs_C5();
+   static LabeledSellaMatrices CreatePrjs_K1();
+   static LabeledSellaMatrices CreatePrjs_K3();
+   static LabeledSellaMatrices CreatePrjs_K5();
    static LabeledSellaMatrices CreatePrjs_M1A();
    static LabeledSellaMatrices CreatePrjs_M1B();
    static LabeledSellaMatrices CreatePrjs_M2A();
@@ -98,11 +90,34 @@ private:
    static LabeledSellaMatrices CreatePrjs_O3();
    static LabeledSellaMatrices CreatePrjs_O4();
    static LabeledSellaMatrices CreatePrjs_O5();
-   static LabeledSellaMatrices CreatePrjs_T1();
-   static LabeledSellaMatrices CreatePrjs_T2();
-   static LabeledSellaMatrices CreatePrjs_T5();
+   static LabeledSellaMatrices CreatePrjs_Q1();
+   static LabeledSellaMatrices CreatePrjs_Q2();
+   static LabeledSellaMatrices CreatePrjs_Q5();
    static LabeledSellaMatrices CreatePrjs_R1();
    static LabeledSellaMatrices CreatePrjs_R3();
+
+private:
+   std::map<std::string, std::vector<S6> > themap;
+   std::vector<LabeledSellaMatrices> projectors;
+   std::vector<LabeledSellaMatrices> perps;
+   StoreResults<std::string, S6> store;
+   static std::vector< std::pair<std::string, MatS6> > vDeloneTypes;
+   StoreResults<std::string, std::string> indexstore;
+
+private:
+   void ProcessItemStoreToVectorMap();
+   void StoreAllReflections(const std::string& label, const S6& s1);
+   void OneBound(const std::string& label, const S6& s1);
+   void ProcessZeros(const std::string& label, const S6& s6);
+   void ProcessVectorMapToPerpsAndProjectors();
+   void Expand(const std::string& label, const MatS6& m);
+
+   static bool FindDuplicate(const std::vector<S6>& out, const S6 s6);
+   static MatS6 ProjectorFromVector(const std::string& label, const S6& s);
+   static S6 MakeSampleType(const MatS6& m);
+   static unsigned long Index6(const unsigned long i, const unsigned long j);
+   static double Fraction(const double d, const S6& s);
+   static std::vector<unsigned long> FindS6Zeros(const S6& s);
 
 };
 
