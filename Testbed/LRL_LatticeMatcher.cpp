3485815b851254d4c3c20004958ddd84d9d7242b
larry andrews <larry6640995@gmail.com>
2019-10-10 08:38:32 -0700

    Lattice matching is now working. Cleanup needed, and does not restore lattice centering yet.

---
+++ Testbed/LRL_LatticeMatcher.cpp
@@ -0,0 +1,184 @@
+#include "LRL_LatticeMatcher.h"
+
+#include <cfloat>
+
+#include "B4.h"
+#include "C3.h"
+#include "G6.h"
+#include "LRL_Cell_Degrees.h"
+#include "LRL_inverse.h"
+#include "S6.h"
+
+#include "Selling.h"
+#include "StoreResults.h"
+#include "TNear.h"
+
+
+LRL_LatticeMatcher::LRL_LatticeMatcher()
+   : m_MVtree()
+   , m_matrixTree()
+   , m_reference(S6())
+   , dcutoff(0.01)
+{
+   FillReflections( );
+   BuildVectorTree( );
+}
+
+void LRL_LatticeMatcher::FillReflections( ) {
+   static const std::vector<MatS6> refl = MatS6::GetReflections( );
+   if (m_matrixTree.empty( ))
+      m_matrixTree.insert( refl );
+}
+
+void LRL_LatticeMatcher::StoreMatS6IfUnique( const MatS6& m) {
+   if (m_matrixTree.NearestNeighbor( dcutoff, m ) == m_matrixTree.end( ))
+      m_matrixTree.insert( m );
+}
+
+std::vector<MatS6> LRL_LatticeMatcher::DoThreeAxes( ) {
+   static std::vector<MatS6> vmp;
+   if (vmp.empty( )) {
+      static const std::vector<std::pair<MatS6, MatS6> > redc = S6::SetUnreductionMatrices( );
+
+      for (size_t i = 0; i < 6; ++i) {
+         const MatS6 mi = redc[i].first;
+         StoreMatS6IfUnique( mi );
+         for (size_t k = 0; k < 6; ++k) {
+            if (i == k) continue;
+            const MatS6 mk = redc[k].first * mi;
+            StoreMatS6IfUnique( mk );
+            for (size_t m = 0; m < 6; ++m) {
+               if (m == k) continue;
+               StoreMatS6IfUnique( redc[m].first );
+            }
+         }
+      }
+   }
+   for (size_t i = 0; i < m_matrixTree.size( ); ++i) vmp.push_back( m_matrixTree[i]);
+   return vmp;
+}
+
+void LRL_LatticeMatcher::BuildVectorTree( void ) {
+   FillReflections( );
+   static const std::vector<MatS6> vMatS6 = DoThreeAxes( );
+   m_matrixTree.insert( vMatS6 );
+   ApplyReflections( vMatS6 );
+}
+
+void LRL_LatticeMatcher::SetReference( const S6& s ) {
+   if (!m_MVtree.empty( ))
+      m_MVtree.clear( );
+   m_reference = s;
+   std::cout << "reference " << s << std::endl << std::endl;
+   BuildReferenceTree( s );
+}
+
+static MatS6 Inverse( const MatS6& min ) {
+   MatS6 m( min );
+   std::vector<double> arout( 36 );
+   inverse( 6, min.GetVector( ).data( ), arout.data( ) );
+   m.SetVector( arout );
+   return m;
+}
+
+
+void LRL_LatticeMatcher::BuildReferenceTree( const S6& s) {
+   double normReference = m_reference.norm( );
+   Scaler_MV scale(s);
+   for (size_t i = 0; i<m_matrixTree.size(); ++i ) {
+      const S6 scaledMV = scale.Scale(m_matrixTree[i] * s);
+      const MV_Pair mv( scaledMV, Inverse( m_matrixTree[i] ) );
+      StoreMV_IfUnique( scaledMV, Inverse( m_matrixTree[i] ) );
+   }
+}
+
+void LRL_LatticeMatcher::ApplyReflections( const std::vector<MatS6>& t ) {
+   static const std::vector<MatS6> refl = MatS6::GetReflections( );
+   for ( size_t i=0; i<t.size(); ++i ) {
+      for ( size_t k=0; k<refl.size(); ++k ){
+         StoreMatS6IfUnique( refl[k] * t[i] );
+      }
+   }
+}
+
+
+S6 LRL_LatticeMatcher::MatchReference( const S6& s ) const {
+   MV_Pair closest;
+   const bool b = m_MVtree.NearestNeighbor( DBL_MAX, closest, MV_Pair(s, MatS6()) );
+   if (!b) throw;
+
+   const double d = (closest.GetMatS6()*s - m_reference).norm( );
+   const int i19191 = 19191;
+   const S6 optimized = closest.GetMatS6( ) * s;
+   ////std::cout << "Reference " << m_reference << std::endl;
+   ////std::cout << "Input " << s << std::endl;
+   ////std::cout << "Difference " << (m_reference - optimized).norm( ) << std::endl;
+   ////std::cout << "Closest " << closest.GetS6() << std::endl;
+   //std::cout << "start next match probe to reference $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+   //std::cout << (m_reference - optimized).norm( ) << " Optimized INITIAL" << optimized << "  " << C3( optimized ) << "  " << LRL_Cell_Degrees( optimized ) << std::endl;
+   //std::cout << B4( optimized ) << std::endl;
+   ////std::cout << "matrix " << std::endl << closest.GetMatS6( ) << std::endl;
+   ////std::cout << "Check " << closest.GetMatS6( ) * closest.GetS6( ) << std::endl;
+   ////std::cout << std::endl;
+
+   // now check for alternate solutions
+   std::vector<MV_Pair> vClosest;
+   S6 s6closest;
+   double dtest_G6;
+   double dmin_G6 = DBL_MAX;
+   const long n = m_MVtree.FindInSphere( 1.01 * d, vClosest, closest );
+   if ( n >= 1 ) {
+      //std::cout << "multiple solutions  n=" << n << std::endl;
+      for ( size_t i=0; i<n; ++i ) {
+         const S6 s6test = vClosest[i].GetMatS6( ) * s;
+         const LRL_Cell_Degrees thecell( s6test );
+         dtest_G6 = (G6(m_reference) - G6( s6test )).norm();
+         if (dmin_G6 >= dtest_G6) {
+            dmin_G6 = dtest_G6;
+            s6closest = s6test;
+         }
+
+         //std::cout << "##" << i << "  " << dtest_G6 << "   " << thecell << std::endl;
+      }
+   }
+   std::cout << dmin_G6 << " Optimized FINAL" << s6closest << "  " << C3( s6closest ) << "  " << LRL_Cell_Degrees( s6closest ) << std::endl;
+   //std::cout << "+++++++++++++++++" << "  " << dmin_G6 << "   " << LRL_Cell_Degrees( s6closest ) << std::endl;
+
+   return s6closest;
+}
+
+void LRL_LatticeMatcher::StoreMV_IfUnique( const S6& s, const MatS6& m ) {
+   MV_Pair scaled_MV( s, m );
+   scaled_MV = MV_Pair::CleanZeros( scaled_MV );
+   /*if (m_MVtree.NearestNeighbor( dcutoff, scaled_MV ) == m_MVtree.end( ))*/ {
+      m_MVtree.insert( scaled_MV );
+   }
+}
+
+MV_Pair MV_Pair::CleanZeros( const MV_Pair& mv ) {
+   MV_Pair t( mv );
+   for (size_t i = 0; i < 6; ++i)
+      t.m_s6[i] = (abs( mv.m_s6[i] ) < 1.0E-10) ? 0.0 : t.m_s6[i];
+   return t;
+}
+
+std::vector<int> MV_Pair::GetOpenIndices( const MV_Pair& mvp ) const {
+   std::vector<int> v;
+   for (size_t i = (size_t)0; i < (size_t)6; ++i) if (!m_mark[i]) v.push_back( (int)i );
+   return v;
+}
+
+
+std::ostream& operator<< ( std::ostream& o, const MV_Pair& v ) {
+   o << "MV_Pair" << std::endl;
+   o << "input S6 " << v.GetS6( ) << "  size " << v.m_originalSize << std::endl;
+   MatS6 mtemp( v.m_ms6 );
+   //o << "---------------" << std::endl;
+   //o << mtemp << std::endl;
+   //o << "---------------" << std::endl;
+   o << "matrix times input " << mtemp * v.GetS6( ) << std::endl;
+   o << "---------------" << std::endl;
+
+   return o;
+}
+
--- Testbed/MAIN_Testbed.cpp
+++ Testbed/MAIN_Testbed.cpp
@@ -1,109 +1,15 @@
 // Testbed.cpp : Defines the entry point for the console application.
 //
 
-#include <iomanip>
-#include <sstream>
-
-#include "ColorTables.h"
-
 #include "CellInputData.h"
-#include "CS6Dist.h"
-#include "G6.h"
-#include "Delone.h"
 #include "LatticeConverter.h"
 #include "LRL_Cell.h"
 #include "LRL_Cell_Degrees.h"
-#include "LRL_CreateFileName.h"
-#include "LRL_DataToSVG.h"
-#include "LRL_inverse.h"
-#include "LRL_MinMaxTools.h"
+#include "LRL_LatticeMatcher.h"
 #include "LRL_ReadLatticeData.h"
 #include "LRL_StringTools.h"
-#include "LRL_ToString.h"
-#include "LRL_MaximaTools.h"
 #include "MatS6.h"
-#include "LRL_MaximaTools.h"
 #include "S6.h"
-
-#include "Selling.h"
-#include "StoreResults.h"
-#include "TNear.h"
-
-
-class Scaler_MV {
-public:
-   Scaler_MV( const S6& s ) :base(s), dbase(base.norm()) {}
-   S6 Scale( const S6& s ) const { return dbase / s.norm( ) * s; }
-   S6 Scale( void ) const { return Scale(base); }
-protected:
-   S6 base;
-   double dbase;
-};
-
-class MV_Pair {
-public:
-   friend std::ostream& operator<< ( std::ostream& o, const C3& v );
-
-   MV_Pair() {}
-   MV_Pair( const S6& s, const MatS6& m ) : m_s6( s ), m_ms6( m ), m_originalSize( s.norm( ) ), m_mark(6,false) {m_ms6 = ResetZeros(m_ms6);}
-   MV_Pair( const MatS6& m, const S6& s ) : m_s6( s ), m_ms6( m ), m_originalSize( s.norm( ) ), m_mark(6,false) {m_ms6 = ResetZeros(m_ms6);}
-   MV_Pair operator- ( const MV_Pair& mv ) const { MV_Pair mvp; mvp.m_s6 = m_s6 - mv.m_s6; return mvp; }
-   MV_Pair operator- ( const S6& s ) const { MV_Pair mvp; mvp.m_s6 = m_s6 - s; return mvp; }
-   double norm( const MV_Pair& mv ) const { return mv.m_s6.norm( ); }
-   double norm( void ) const { return m_s6.norm( ); }
-   S6 GetS6( void ) const { return m_s6; }
-   MatS6 GetMatS6( void ) const { return m_ms6; }
-   double GetSize( void ) const {return m_originalSize;}
-   static MatS6 ResetZeros( const MatS6& m ) { MatS6 mt( m ); for (size_t i = 0; i < 6; ++i) if (m[i] == 0.0) mt[i] = 0.0; return mt; }
-   static MV_Pair CleanZeros( const MV_Pair& mv ) { 
-      MV_Pair t( mv ); 
-      for (size_t i=0; i<6; ++i ) 
-         t.m_s6[i] = (abs( mv.m_s6[i] ) < 1.0E-10) ?  0.0 : t.m_s6[i];
-      return t;
-   }
-   void SetMark( const int i ) { m_mark[i] = true; }
-   bool GetMark( const int i ) const { return m_mark[i]; }
-   bool ClearMarks( void ) { for (size_t i = 0; i < 6; ++i) m_mark[i] = false; }
-   std::vector<int> GetOpenIndeces( const MV_Pair& mvp ) const { 
-      std::vector<int> v;
-      for (size_t i = 0; i < 6; ++i) if (!m_mark[i]) v.push_back( i ); 
-      return v;
-   }
-public:
-   S6 m_s6;
-   MatS6 m_ms6;
-   double m_originalSize;
-   std::vector<bool> m_mark;
-};
-
-std::ostream& operator<< ( std::ostream& o, const MV_Pair& v ) {
-   o << "MV_Pair" << std::endl;
-   o << "input S6 " << v.GetS6( ) << "  size " << v.m_originalSize << std::endl;
-   MatS6 mtemp( v.m_ms6 );
-   //o << "---------------" << std::endl;
-   //o << mtemp << std::endl;
-   //o << "---------------" << std::endl;
-   o << "matrix times input " <<  mtemp * v.GetS6( ) << std::endl;
-   o << "---------------" << std::endl;
-
-   return o;
-}
-
-
-CNearTree<MV_Pair> mvtree;
-
-static const double dcutoff = 0.01;
-
-
-
-static MatS6 Inverse( const MatS6& min ) {
-   MatS6 m( min );
-   std::vector<double> arout( 36 );
-   inverse( 6, min.GetVector( ).data( ), arout.data( ) );
-   m.SetVector( arout );
-   return m;
-}
-
 
 std::string Letters( void ) {
    return "V,G,D,S,P,A,B,C,I,F,R,C3,G6,S6,B4,D7,H";
@@ -146,160 +52,20 @@ std::vector<S6>  GetReducedInput( ) {
    return vLat;
 }
 
-void StoreMV_IfUnique( const S6& s, const MatS6& m ) {
-   MV_Pair scaled_MV( s, m );
-   scaled_MV = MV_Pair::CleanZeros( scaled_MV );
-   if (mvtree.NearestNeighbor( dcutoff, scaled_MV ) == mvtree.end( )) {
-      mvtree.insert( scaled_MV );
-   }
-}
-
-void ExpandReflections_MV( const MV_Pair& mv ) {
-   static const std::vector<MatS6> refl = MatS6::GetReflections( );
-   const Scaler_MV scale( mv.GetS6() );
-   for (size_t i = 0; i < refl.size( ); ++i) {
-      //std::cout << "p  " << LRL_Cell_Degrees(refl[i] * s) << std::endl;
-      const S6 scaledS6 = scale.Scale( refl[i] * mv.GetS6() );
-      StoreMV_IfUnique( scaledS6, mv.GetMatS6() * Inverse( refl[i] ) );
-   }
-}
-
-void ExpandReflections_MV( const S6& s ) {
-   static const std::vector<MatS6> refl = MatS6::GetReflections( );
-   const Scaler_MV scale(s);
-   for (size_t i = 0; i < refl.size( ); ++i) {
-      const S6 scaledS6 = scale.Scale( refl[i] * s);
-      StoreMV_IfUnique( scaledS6, Inverse( refl[i] ) );
-   }
-}
-
-void ExpandBoundaries_MV( ) {
-   static const std::vector<std::pair<MatS6, MatS6> > redc = S6::SetUnreductionMatrices( );
-
-   std::vector<S6> v1;
-   std::vector<MatS6> m1;
-   for (size_t i = 0; i < mvtree.size( ); ++i) {
-      v1.push_back( mvtree[i].GetS6( ) );
-      m1.push_back( ( mvtree[i].GetMatS6( ) ) );
-   }
-
-   const Scaler_MV scale( v1[0] );
-
-   for (size_t k = 0; k < v1.size( ); ++k) {
-      for (size_t i = 0; i < redc.size( ); ++i) {
-         const S6 scaledS6 = scale.Scale( redc[i].first * v1[k] );
-         //const  MatS6 totalTransform = redc[i].first * m1[k];
-
-         //StoreMV_IfUnique( scaledS6, Inverse( totalTransform ) );
-         //const MV_Pair scaled_MV( scaledS6, Inverse( totalTransform ) );
-         //if (mvtree.NearestNeighbor( dcutoff, scaled_MV ) == mvtree.end( )) {
-         //   mvtree.insert( scaled_MV );
-         //}
-         const  MatS6 totalTransform = m1[k] * Inverse( redc[i].first );
-         const MV_Pair scaled_MV( scaledS6, totalTransform );
-         if (mvtree.NearestNeighbor( dcutoff, scaled_MV ) == mvtree.end( )) {
-            mvtree.insert( scaled_MV );
-         }
-      }
-   }
-}
-
-void Expand( const S6& s ) {
-   ExpandReflections_MV( s );
-
-   ExpandBoundaries_MV( );
-}
-
-
-   void DoThreeAxes( ) {
-      std::vector<int> out;
-      int count1 = 0;
-      int count2 = 0;
-      int count3 = 0;
-      for (size_t i = 0; i < 6; ++i) {
-         std::cout << i << std::endl;
-         ++count1;
-         for (size_t k = 0; k < 6; ++k) {
-            if (i == k) continue;
-            ++count2;
-            std::cout << i << k << std::endl;
-            for (size_t m = 0; m < 6; ++m) {
-               if (m == i || m == k) continue;
-               std::cout << i << k << m << std::endl;
-               ++count3;
-            }
-         }
-      }
-      std::cout << "counts = " << count1 << "  " << count2 << "  " << count3 << std::endl;
-   }
-
-//void Expand_MV_2( const std::string& label, const MV_Pair& mvp ) {
-//   const S6 s6( mvp.GetS6( ) );
-//   if (s6.IsValid( )) {
-//      switch (s6.CountZeros( )) {
-//      case 0:
-//         ExpandReflections_MV( mvp );
-//         break;
-//      case 1:
-//         OneBound( label, mvp );
-//         break;
-//      case 2:
-//      case 3:
-//         ProcessZeros( label, mvp );
-//         break;
-//      default:
-//         throw "this should never happen";
-//         break;
-//      }
-//   }
-//   //store.ShowResults();
-//}
-
-void ReportMultiMatch( const Scaler_MV& scaler, const double closestDistance, const S6& toFind, const std::vector<MV_Pair>& sphere ) {
-   static const MatS6 constMatS6 = MatS6( );
-   std::cout << "closestDistance " << closestDistance << std::endl;
-   for ( size_t i=0; i<sphere.size(); ++i ) {
-      std::cout << i << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
-      std::cout << sphere[i] << std::endl;
-      std::cout << "  d=" << closestDistance <<
-         "     " << sphere[i].GetMatS6( ) * toFind << "      " << LRL_Cell_Degrees( sphere[i].GetMatS6( ) * toFind ) << std::endl;
-   }
-}
-
-void ReportOneMatch( const Scaler_MV& scaler, const S6& toFind ) {
-   static const MatS6 constMatS6 = MatS6( );
-   CNearTree<MV_Pair>::iterator it_MV = mvtree.NearestNeighbor( 100000., MV_Pair( toFind, constMatS6 ) );
-   const double closestDistance = (scaler.Scale( toFind ) - (*it_MV).GetS6( )).norm( );
-   std::vector<MV_Pair> sphere;
-   const long n = mvtree.FindInSphere( 1.01 * closestDistance, sphere, MV_Pair( toFind, constMatS6 ) );
-   if (n == 1) {
-      std::cout << "tree index " << it_MV.get_position( ) << "  " << closestDistance <<
-         "     " << (*it_MV).GetMatS6( ) * toFind << "      " << LRL_Cell_Degrees( (*it_MV).GetMatS6( ) * toFind ) << std::endl;
-   }
-   else {
-      std::cout << "WARNING ######################################################################## found more than one match  n=" << n << std::endl;
-      ReportMultiMatch( scaler, closestDistance, toFind, sphere );
-   }
-}
-
-void DoAllMatches( const std::vector<S6>& vLat ) {
-   const Scaler_MV scaler( vLat[0] );
-
-   for (size_t i = 0; i < vLat.size( ); ++i) {
-      std::cout << std::endl << "search item index " << i << std::endl;
-      ReportOneMatch( scaler, vLat[i] );
-      std::cout << "===================================================================================================" << std::endl;
-   }
-}
-
 int main( int argc, char* argv[] )
 {
-   DoThreeAxes( );
-   const std::vector<S6> vLat =  GetReducedInput( );
+   const std::vector<LRL_ReadLatticeData> input = GetInputCells( );
+   const std::vector<S6> vLat = GetInputSellingReducedVectors( input );
+   if (vLat.size( ) > 0) {
+      LRL_LatticeMatcher lm;
+      std::cout << "MV tree size = " << lm.size( ) << std::endl;
+      lm.SetReference( vLat[0] );
 
-   Expand( vLat[0] );
-
-   DoAllMatches( vLat );
+      for (size_t lat = 0; lat < vLat.size( ); ++lat) {
+         const S6 s1 = lm.MatchReference( vLat[lat] );
+         //std::cout << "=============================" << std::endl;
+      }
+   }
 
    return 0;
 }
@@ -309,7 +75,7 @@ int main( int argc, char* argv[] )
 f 10 10.001 10.002  89 89 89
 f 10 10 10  90 90 90
 f 11 10 10  90 90 90
-f 10 11 10  90 90 90  this is the problem one
+f 10 11 10  90 90 90  this is the problem one 
 f 10 10 11  90 90 90
 end
 
@@ -332,10 +98,14 @@ end
 
 
 f 10 10 10  90 90 90
-f 10 10 10  90 90 90
 f 11 10 10  90 90 90
 f 10 11 10  90 90 90  this is the problem one
 f 10 10 11  90 90 90
+end
+
+f 10 10 10  90 90 90
+f 11 10 10  90 90 90
+f 10 11 10  90 90 90  this is the problem one
 end
 
 
