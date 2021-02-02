932eca0f1e912db64c5f713ec3d744c467c71d8f
larry andrews <larry6640995@gmail.com>
2021-02-02 11:56:27 -0800

    Dirichlet code -- change to  add  more global constants and output to a file instead of cout. Also now allows multiple cells input (which go to separate output files)

--- Dirichlet/Dirichlet.h
+++ Dirichlet/Dirichlet.h
@@ -114,6 +114,8 @@ public:
 
 
    static LRL_Cell ReadAndReduceCell();
+   static LRL_Cell ParseAndReduceStringCellWithLattice(const std::string& strCell);
+   static LRL_Cell ParseAndReduceCell(const std::string& lattice, const std::string& strCell);
    std::vector<Vector_3> CreateVectorOfLatticePoints();
    std::vector<Dirichlet_Intersections> GenerateIntersectionList();
    std::vector<Dirichlet_Intersections> CleanIntersectionList(const std::vector<Dirichlet_Intersections>& vInter);
--- Dirichlet/ParseData.h
+++ Dirichlet/ParseData.h
@@ -5,15 +5,19 @@
 
 class ParseData {
 public:
-   std::string m_label;
-   std::string m_dataTypeToRead;
-   void* m_dataLocation;
    ParseData(const std::string& label, const std::string& dataType, void* dataLocation)
       : m_label(label)
       , m_dataTypeToRead(dataType)
       , m_dataLocation(dataLocation)
    {
    };
+   std::string GetLabel() const { return m_label; }
+   std::string GetDataType() const { return m_dataTypeToRead; }
+   void* GetLocation() const { return m_dataLocation; }
+private:
+   std::string m_label;
+   std::string m_dataTypeToRead;
+   void* m_dataLocation;
 };
 
 #endif // PARSEDATA_H
