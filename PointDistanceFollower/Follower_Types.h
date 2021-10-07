3b3a254afb73859b59d439c44456b5983dc98860
larry andrews <larry6640995@gmail.com>
2021-10-07 07:11:49 -0700

    Add Grassmannian figure code, not very gerneral functions In PointDistanceFollower, work on refactoring of the distance types

--- PointDistanceFollower/Follower_Types.h
+++ PointDistanceFollower/Follower_Types.h
@@ -5,35 +5,44 @@
 #include "D13.h"
 #include "MultiFollower.h"
 
-template<typename T>
-class Follower_Type_Base {
-public:
-  virtual  std::vector<std::pair<T, T> > GetPath(void) { return m_Path; }
-  virtual MultiFollower CalculateDistances(const MultiFollower& mf) const = 0;
-  virtual void SetLatticePointChoiceForDistanceCalculation() = 0;
-  void SetDistance6(const std::vector<double>& v) { LRL_Path<T>::SetDistances(v); }
 
-protected:
-   std::vector<std::pair<T,T> >m_Path;
-   std::string m_name;
-   void SetComputeStartTime(void) { m_ComputeStartTime = std::clock(); }
-   std::clock_t GetComputeStartTime(void) const { return m_ComputeStartTime; }
-   void SetTime2ComputeFrame(const double computeTime) {
-      m_seconds2ComputerFrame = computeTime;
-   }
-   double m_seconds2ComputerFrame;
-   double GetTime2ComputeFrame() const { return m_seconds2ComputerFrame; }
-   std::clock_t GetComputeStartTime(void) const {
-      return m_ComputeStartTime
-};
 
-class Follower_Type_D13 : public Follower_Type_Base<D13> {
+class Follower_Type_D13 {
 public:
    Follower_Type_D13();
 
    MultiFollower CalculateDistances(const MultiFollower& mf) const;
    void SetLatticePointChoiceForDistanceCalculation();
 protected:
+};
+
+class Follower_Type {
+public:
+   Follower_Type(const Follower_Type_D13);
+  //virtual  std::vector<std::pair<T, T> > GetPath(void) { return m_Path; }
+  MultiFollower CalculateDistances(const MultiFollower& mf) const;
+  //virtual void SetLatticePointChoiceForDistanceCalculation() = 0;
+  //void SetDistance6(const std::vector<double>& v) { LRL_Path<T>::SetDistances(v); }
+
+protected:
+   // data
+   std::vector<std::pair<S6, S6> >m_SellingPath;
+   std::vector<std::pair<G6, G6> >m_NiggliPath;
+   std::vector<std::pair<D13, D13> >m_D13Path;
+   std::string m_name;
+   std::clock_t m_ComputeStartTime;
+   double m_seconds2ComputerFrame;
+
+   // functions
+   void SetComputeStartTime(void) { m_ComputeStartTime = std::clock(); }
+   std::clock_t GetComputeStartTime(void) const { return m_ComputeStartTime; }
+   void SetTime2ComputeFrame(const double computeTime) {
+      m_seconds2ComputerFrame = computeTime;
+   }
+   double GetTime2ComputeFrame() const { return m_seconds2ComputerFrame; }
+
+private:
+   Follower_Type();  // do not implement
 };
 
 
