2ecad2ff51fa3adefe3db5b5958ed402cdee1faf
larry andrews <larry6640995@gmail.com>
2021-10-11 13:41:35 -0700

    cmdToB4 -- add writing out the 4 vectors

--- CmdToB4/CmdToB4.cpp
+++ CmdToB4/CmdToB4.cpp
@@ -9,11 +9,16 @@
 int main()
 {
    const std::vector<LRL_ReadLatticeData> inputList = LRL_ReadLatticeData().ReadLatticeData();
-   std::cout << "; To B4 lengths" << std::endl;
+   std::cout << "; To B4" << std::endl;
 
    for (size_t i = 0; i < inputList.size(); ++i) {
       const B4 b4(inputList[i].GetCell());
+      std::cout 
+         << "a: " << b4[0] << std::endl 
+         << "b: " << b4[1] << std::endl 
+         << "c: " << b4[2] << std::endl 
+         << "d: " << b4[3] << std::endl;
       std::cout << "B4 lengths   " << b4[0].norm() << " " << b4[1].norm() << " " << b4[2].norm() << " " << b4[3].norm();
-      std::cout << std::endl;
+      std::cout << std::endl << std::endl;
    }
 }
