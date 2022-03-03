838de6a7abc939a0550c7f309c102c42f81315b6
larry andrews <larry6640995@gmail.com>
2022-03-03 07:08:32 -0800

    SELLA -- preparing output and data for re-centering lattices

--- LatticeRepresentationLib/DeloneType.h
+++ LatticeRepresentationLib/DeloneType.h
@@ -15,23 +15,27 @@
 
 class DeloneFitResults {
 public:
-   friend std::ostream& operator<< ( std::ostream&, const DeloneFitResults& );
+   friend std::ostream& operator<< (std::ostream&, const DeloneFitResults&);
 
-   DeloneFitResults( const double fit, const S6& bestApprox, const S6& perpv, const MatS6& toCanon );
-   void SetZscore( const double score ) { m_zscore = score; }
-   void SetLatticeType( const std::string& s ) { m_latticeType = s; }
-   double GetRawFit( void ) const { return m_rawFit; }
-   double GetZscore( void ) const { return m_zscore; }
-   S6 GetBestFit( void ) const { return m_bestFit; }
-   S6 GetDifference( void ) const { return m_difference; }
-   MatS6 GetToCanon( void ) const { return m_toCanonicalDeloneType; }
-   std::string GetLatticeType( void ) const { return m_latticeType; }
+   DeloneFitResults(const double fit, const S6& bestApprox, const S6& perpv, const MatS6& toCanon);
+   void SetZscore(const double score) { m_zscore = score; }
+   void SetLatticeType(const std::string& s) { m_latticeType = s; }
+   double GetRawFit(void) const { return m_rawFit; }
+   double GetZscore(void) const { return m_zscore; }
+   S6 GetBestFit(void) const { return m_bestFit; }
+   S6 GetDifference(void) const { return m_difference; }
+   MatS6 GetToCanon(void) const { return m_toCanonicalDeloneType; }
+   std::string GetLatticeType(void) const { return m_latticeType; }
    void SetReductionMatrix(const MatS6& matrix) { m_reductionMatrix = matrix; }
    MatS6 GetReductionMatrix() const { return m_reductionMatrix; }
+   void SetOriginalInput(const S6& s6) { m_originalInput = s6; }
+   S6 GetOriginalInput() const {return m_originalInput;}
+
 protected:
    std::string m_latticeType;
    double m_rawFit;
    double m_zscore;
+   S6 m_originalInput;
    S6 m_bestFit;
    S6 m_difference;
    MatS6 m_toCanonicalDeloneType;
