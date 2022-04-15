ce29d58014be36d1f17505ce7e0cc550d912c851
larry <larry6640995@gmail.com>
2022-04-15 15:36:14 -0700

    Final setup for working code with input commented

---
+++ LatticeRepresentationLib/MatricesForOneDeloneType.cpp
@@ -0,0 +1,1 @@
+#include "MatricesForOneDeloneType.h"
--- cmdgen/CmdGen.cpp
+++ cmdgen/CmdGen.cpp
@@ -1,13 +1,35 @@
 
 #include <iostream>
 
-#include"LatticeConverter.h"
-#include "LRL_ReadLatticeData.h"
 #include "G6.h"
+#include "LRL_Cell.h"
 #include "Niggli.h"
 #include "GenerateLatticeTypeExamples.h"
 
+// how many examples of each type to generate
 static const int ngen = 1;
+
+/*
+The "name" variable is the designator for the Bravais type(s)
+you want to generate. Because the code below works in Niggli
+space, the name should be one of the following types:
+1) The number of a single type from the International Tables (1-44)
+2) Bravais type, such as cI for body-centered cubic or oI for
+body centered orthorhombic. Because there are multiple oI
+cases, all will be used. (case sensitive) Hexagonal is hP,
+and rhombohedral is hR.
+3) Crystal system such as: c, t, h, o, m, a for cubic,
+tetragonal, rhombohedral, hexagonal, orthorhombic, monoclinic,
+anorthic (=triclinic). (case sensitive)
+4) Empty string (or unrecognised) delivers all!!
+
+For centered types, the output will be the centered, not the
+primitive, cell.
+
+Obviously, these two parameters could be made into input command
+parameters. Values 5 cI would be obvious, but what is 10 15 
+supposed to mean?
+*/
 static std::string name = "cI"; // blank or unrecognized gives all types
 
 
@@ -23,9 +45,6 @@ G6 TryToGetAGoodProjection(const std::shared_ptr<GenerateNiggliBase>& pt,
       Niggli::Reduce(probe, probe);
       ++count;
    }
-   //std::cout << "probe in TryToGetAGoodProjection "
-   //   << probe << std::endl
-   //   << LRL_Cell_Degrees(probe) << std::endl << std::endl;
    return probe;
 }
 
@@ -33,11 +52,6 @@ G6 Generate(const std::shared_ptr<GenerateNiggliBase>& pt) {
    const G6 probe = TryToGetAGoodProjection(pt, pt->GetPrj(), 50);
 
    const G6 test = pt->GetToCenter() * probe;
-   //std::cout
-   //   << pt->GetBravaisType() << " "
-   //   << pt->GetITNum() << " "
-   //   << LRL_Cell_Degrees(test)
-   //   << std::endl;
    return probe;;
 }
 
@@ -52,7 +66,7 @@ int main()
       for (size_t i = 0; i < ngen; ++i) {
          const G6 g = Generate(vglb[lat]);
          std::cout << "G6 "
-            << (g) << " "
+            << g << " "
             << " IT# = " << pt->GetITNumber() << "  "
             << pt->GetBravaisType()
             << std::endl;
