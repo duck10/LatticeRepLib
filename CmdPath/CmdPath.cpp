57d54825ed1243430cbc7fe15b59238e5e2891f1
larry <larry6640995@gmail.com>
2023-01-13 12:53:38 -0800

    CmdDist - output final input lattice (which has not following lattice for distance calculation) CmdPath - correct behavior if more than 2 lattices entered

--- CmdPath/CmdPath.cpp
+++ CmdPath/CmdPath.cpp
@@ -10,7 +10,8 @@
 
 std::vector<S6> CreatePathBetweenTwoPoints(const int npts, const S6& delta,  const S6& s1, const S6& s2) {
    std::vector<S6> out(1,s1);
-
+   //std::cout << "s1 " << s1 << "  s2 " << s2 << std::endl;
+   std::cout << "delta " << delta << std::endl;
    for (int i = 0; i < npts-1; ++i) {
       const S6 diff = double(i+1) * delta;
       const S6 temp = s1 + diff;
@@ -52,12 +53,10 @@ int main(int argc, char* argv[])
     }
 
    std::vector<S6> path;
-   //std::cout << "npts " << npts << std::endl;
-   const S6& s1(s6Cells[0]);
-   const S6& s2(s6Cells.back());
-   const S6 delta  = (s2 - s1) / double(pathLength-1);
 
-   for (int i = 0; i < s6Cells.size()-1; ++i) {
+   for (size_t i = 0; i < s6Cells.size()-1; ++i) {
+      const S6 delta = (s6Cells[i + 1] - s6Cells[i]) / double(pathLength - 1);
+      std::cout << i << " s6c1 " << s6Cells[i] << "  s6c2 " << s6Cells[i + 1] << std::endl;
       const std::vector<S6> pathStep = CreatePathBetweenTwoPoints(pathLength, delta, s6Cells[i], s6Cells[i+1]);
       path.insert(path.end(), pathStep.begin(), pathStep.end());
    }
