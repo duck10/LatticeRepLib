d51721b51d91ad1a396250c023a622aaf59f242d
larry <larry6640995@gmail.com>
2023-10-31 11:34:46 -0700

    PlotC3 -- move text output from SVG to stdout

--- PlotC3/PlotC3.cpp
+++ PlotC3/PlotC3.cpp
@@ -102,33 +102,102 @@ std::string CellPrecision2(const S6& s) {
 }
 
 std::string PrepareLegend(const double x, const double y, const std::vector<S6>& v) {
+   //std::string out;
+   //const std::string xtext = LRL_ToString(x);
+   //const std::string ytext = LRL_ToString(y);
+   //const std::string sourceFile(__FILE__);
+   //const std::string sxlabel = LRL_ToString(x + 20);
+   //const std::string sylabel = LRL_ToString(y + 50);
+
+   //const std::string sydate = LRL_ToString(y+100);
+   //const std::string sycount = LRL_ToString(y + 150);
+
+   //const std::string fileLabel = "<text x=\"" + sxlabel + "\" y=\""+ sylabel+"\" >"
+   //   + sourceFile + " </text>\n";
+
+   //const std::string count = "<text x=\"" + sxlabel + "\" y=\""
+   //   + sycount + "\" > " + "Number of points: " + LRL_ToString(v.size()) + "</text>\n";
+
+   //const std::string rect = 
+   //   "<rect x=\""+xtext+"\" y= \"" + ytext + "\" width = \"500\"" +" height=\"500\" fill=\"none\" "
+   //   "stroke=\"black\" stroke-width=\"2\" />\n";
+
+   //const std::string dateTime = LRL_CreateFileName::Create("Created ", "");
+   //const std::string datePos = LRL_ToString(y + 460);
+   //const std::string date = "<text x=\"" + sxlabel + "\" y=\"" + datePos + "\">" + dateTime + "</text>\n";
+
+   //out += fileLabel;
+   //out += date;
+   //out += rect;
+   //out += count;
+
+   //if (v.empty()) {
+   //   std::cout << "; PlotC3 requires at least one lattice" << std::endl;
+   //}
+   //else if (v.size() < 5) {
+   //   // just list them all
+   //   for (size_t i = 0; i < v.size(); ++i) {
+   //      const std::string ypos = LRL_ToString(y + 200 + i * 20);
+   //      const std::string cellText =
+   //         "<text x=\"" + sxlabel + "\" y=\""
+   //         + ypos + "\" > " + "P " + CellPrecision2(v[i]) + "</text>\n";
+   //      out += cellText;
+   //   }
+   //}
+   //else {
+   //   // just list the first 3 and the last one
+   //   for (size_t i = 0; i < 3; ++i) {
+   //      const std::string ypos = LRL_ToString(y + 200 + i * 20);
+   //      const std::string cellText1 =
+   //         "<text x=\"" + sxlabel + "\" y=\""
+   //         + ypos + "\" > " + "P " + CellPrecision2(v[i]) + "</text>\n";
+
+   //      out += cellText1;
+   //      out += "...\n";
+   //   }
+   //   const std::string ypos = LRL_ToString(y + 280);
+   //   std::stringstream os;
+   //   os << CellPrecision2(v[v.size() - 1]);
+
+   //   const std::string cellText2 =
+   //      "<text x=\"" + sxlabel + "\" y=\""
+   //      + ypos + "\" > " + "P " + os.str() + "</text>\n";
+   //   const std::string dots =
+   //      "<text x=\"" + sxlabel + "\" y=\""
+   //      + LRL_ToString(y + 255) + "\" > " + "\n ...\n</text>\n";
+   //   out += dots;
+   //   out += cellText2;
+
+   //}
+   //const std::string ypos = LRL_ToString(y + 3400);
+   //const std::string commandLine =
+   //   "<text x=\"" + sxlabel + "\" y=\""
+   //   + LRL_ToString(y + 355) + "\" > " + "\n edit SVG file to insert command line text</text>\n";
+
+   //out += commandLine;
    std::string out;
    const std::string xtext = LRL_ToString(x);
    const std::string ytext = LRL_ToString(y);
    const std::string sourceFile(__FILE__);
-   const std::string sxlabel = LRL_ToString(x + 20);
-   const std::string sylabel = LRL_ToString(y + 50);
+   //const std::string sxlabel = LRL_ToString(x + 20);
+   //const std::string sylabel = LRL_ToString(y + 50);
 
-   const std::string sydate = LRL_ToString(y+100);
-   const std::string sycount = LRL_ToString(y + 150);
+   //const std::string sydate = LRL_ToString(y + 100);
+   //const std::string sycount = LRL_ToString(y + 150);
 
-   const std::string fileLabel = "<text x=\"" + sxlabel + "\" y=\""+ sylabel+"\" >"
-      + sourceFile + " </text>\n";
+   const std::string fileLabel = sourceFile + "\n";
 
-   const std::string count = "<text x=\"" + sxlabel + "\" y=\""
-      + sycount + "\" > " + "Number of points: " + LRL_ToString(v.size()) + "</text>\n";
+   const std::string count =  "Number of points: " + LRL_ToString(v.size()) + "\n\n";
 
-   const std::string rect = 
-      "<rect x=\""+xtext+"\" y= \"" + ytext + "\" width = \"500\"" +" height=\"500\" fill=\"none\" "
-      "stroke=\"black\" stroke-width=\"2\" />\n";
+      //   const std::string rect = 
+      //     "<rect x=\""+xtext+"\" y= \"" + ytext + "\" width = \"500\"" +" height=\"500\" fill=\"none\" "
+      //     "stroke=\"black\" stroke-width=\"2\" />\n";
 
-   const std::string dateTime = LRL_CreateFileName::Create("Created ", "");
-   const std::string datePos = LRL_ToString(y + 460);
-   const std::string date = "<text x=\"" + sxlabel + "\" y=\"" + datePos + "\">" + dateTime + "</text>\n";
+   const std::string date = LRL_CreateFileName::Create("Created ", "\n");
 
    out += fileLabel;
    out += date;
-   out += rect;
+   //   out += rect;
    out += count;
 
    if (v.empty()) {
@@ -137,50 +206,38 @@ std::string PrepareLegend(const double x, const double y, const std::vector<S6>&
    else if (v.size() < 5) {
       // just list them all
       for (size_t i = 0; i < v.size(); ++i) {
-         const std::string ypos = LRL_ToString(y + 200 + i * 20);
-         const std::string cellText =
-            "<text x=\"" + sxlabel + "\" y=\""
-            + ypos + "\" > " + "P " + CellPrecision2(v[i]) + "</text>\n";
+         const std::string cellText =  "P " + CellPrecision2(v[i]) + "\n";
          out += cellText;
       }
    }
    else {
       // just list the first 3 and the last one
       for (size_t i = 0; i < 3; ++i) {
-         const std::string ypos = LRL_ToString(y + 200 + i * 20);
-         const std::string cellText1 =
-            "<text x=\"" + sxlabel + "\" y=\""
-            + ypos + "\" > " + "P " + CellPrecision2(v[i]) + "</text>\n";
+         const std::string cellText1 = "P " + CellPrecision2(v[i]);
 
-         out += cellText1;
-         out += "...\n";
+            out += cellText1 + "\n";
       }
-      const std::string ypos = LRL_ToString(y + 280);
       std::stringstream os;
       os << CellPrecision2(v[v.size() - 1]);
 
-      const std::string cellText2 =
-         "<text x=\"" + sxlabel + "\" y=\""
-         + ypos + "\" > " + "P " + os.str() + "</text>\n";
-      const std::string dots =
-         "<text x=\"" + sxlabel + "\" y=\""
-         + LRL_ToString(y + 255) + "\" > " + "\n ...\n</text>\n";
+      const std::string dots = "\n ...\n\n";
+
+
+      const std::string cellText2 = "  P " + os.str() + "\n";
       out += dots;
       out += cellText2;
 
    }
    const std::string ypos = LRL_ToString(y + 3400);
-   const std::string commandLine =
-      "<text x=\"" + sxlabel + "\" y=\""
-      + LRL_ToString(y + 355) + "\" > " + "\n edit SVG file to insert command line text</text>\n";
+   const std::string commandLine = LRL_ToString("\n edit SVG file to insert command line text\n");
 
-   //out += commandLine;
-
-   return out;
+   std::cout << out << std::endl;
+   return "";
 }
 
 int main(int argc, char* argv[])
 {
+   std::cout << "; PlotC3" << std::endl;
    const std::string filename = LRL_CreateFileName::Create("PLT_", "svg");
 
    C3Plot c3plot(filename, 1800, 800, 500, 500);
