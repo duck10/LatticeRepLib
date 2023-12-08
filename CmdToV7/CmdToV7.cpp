89986d295e05dccd0f819360d4bf6bcda0939575
larry <larry6640995@gmail.com>
2023-12-08 12:36:04 -0800

    changes to fix CmdGen choices update V7 and disable work in progress for V7 input CmdToS6L -- improve output

--- CmdToV7/CmdToV7.cpp
+++ CmdToV7/CmdToV7.cpp
@@ -7,44 +7,45 @@
 #include "LRL_Cell.h"
 #include "LRL_ReadLatticeData.h"
 #include "Niggli.h"
+#include "V7.h"
+//
+//class V7 /*: private BasisBase<G6> */{
+//public:
+//   friend std::ostream& operator<< (std::ostream&, const V7&);
+//   V7() {};
+//   V7(const std::string& lattice, const LRL_Cell& cell);
+//private:
+//   std::vector<double> m_vec;
+//   bool m_valid;
+//};
+//
+//std::ostream& operator<< (std::ostream& o, const V7& v) {
+//   for (size_t i = 0; i < 7; ++i) {
+//      o << v.m_vec[i] << " ";
+//   }
+//   std::cout << std::endl;
+//   return o;
+//}
 
-class V7 /*: private BasisBase<G6> */{
-public:
-   friend std::ostream& operator<< (std::ostream&, const V7&);
-   V7() {};
-   V7(const std::string& lattice, const LRL_Cell& cell);
-private:
-   std::vector<double> m_vec;
-   bool m_valid;
-};
 
-std::ostream& operator<< (std::ostream& o, const V7& v) {
-   for (size_t i = 0; i < 7; ++i) {
-      o << v.m_vec[i] << " ";
-   }
-   std::cout << std::endl;
-   return o;
-}
-
-
-V7::V7(const std::string& lattice, const LRL_Cell& cell)
-   : m_valid(true)
-{
-   m_vec.resize(7);
-   const LRL_Cell pCell = LatticeConverter::MakePrimitiveCell(lattice, cell);
-   G6 red;
-   G6 invRed;
-   const bool b1 = Niggli::Reduce(cell, red);
-   const bool b2 = Niggli::Reduce(cell.Inverse(), invRed);
-   const double volume = cell.Volume();
-   m_vec[0] = sqrt(red[0]);
-   m_vec[1] = sqrt(red[1]);
-   m_vec[2] = sqrt(red[2]);
-   m_vec[3] = 1.0 / sqrt(invRed[0]);
-   m_vec[4] = 1.0 / sqrt(invRed[1]);
-   m_vec[5] = 1.0 / sqrt(invRed[2]);
-   m_vec[6] = pow(volume, 1.0 / 3.0);
-}
+//V7::V7(const std::string& lattice, const LRL_Cell& cell)
+//   : m_valid(true)
+//{
+//   m_vec.resize(7);
+//   const LRL_Cell pCell = LatticeConverter::MakePrimitiveCell(lattice, cell);
+//   G6 red;
+//   G6 invRed;
+//   const bool b1 = Niggli::Reduce(cell, red);
+//   const bool b2 = Niggli::Reduce(cell.Inverse(), invRed);
+//   const double volume = cell.Volume();
+//   m_vec[0] = sqrt(red[0]);
+//   m_vec[1] = sqrt(red[1]);
+//   m_vec[2] = sqrt(red[2]);
+//   m_vec[3] = 1.0 / sqrt(invRed[0]);
+//   m_vec[4] = 1.0 / sqrt(invRed[1]);
+//   m_vec[5] = 1.0 / sqrt(invRed[2]);
+//   m_vec[6] = pow(volume, 1.0 / 3.0);
+//}
 
 int main()
 {
@@ -53,6 +54,6 @@ int main()
 
    for (size_t i = 0; i < inputList.size(); ++i) {
       const LRL_Cell pCell = LatticeConverter::MakePrimitiveCell(inputList[i].GetLattice(), inputList[i].GetCell());
-      std::cout << "V7 " << V7("P ", pCell) << std::endl;
+      std::cout << "V7 " << V7(pCell) << std::endl;
    }
 }
