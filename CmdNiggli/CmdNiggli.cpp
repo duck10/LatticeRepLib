1ffb45dd80cd12eac6545e72f6d0735f50ce367b
larry <larry6640995@gmail.com>
2025-01-27 09:21:08 -0800

    CmdNiggli -- updated to use ProgramSetup.

--- CmdNiggli/CmdNiggli.cpp
+++ CmdNiggli/CmdNiggli.cpp
@@ -1,20 +1,38 @@
-
 #include <iostream>
 
-#include"LatticeConverter.h"
-#include "LRL_ReadLatticeData.h"
+#include "CmdNiggliControls.h"
 #include "G6.h"
+#include "LatticeConverter.h"
+#include "LRL_ReadLatticeData.h"
 #include "Niggli.h"
+#include "ProgramSetup.h"
 
-int main()
-{
+int main() {
    std::cout << "; Niggli reduced" << std::endl;
-   const std::vector<LRL_ReadLatticeData> inputList = LRL_ReadLatticeData().ReadLatticeData();
-   for (const auto& input : inputList) {
-      const LRL_Cell pCell = LatticeConverter::MakePrimitiveCell(input.GetLattice(), input.GetCell());
-      G6 g;
-      const bool b = Niggli::Reduce(G6(pCell), g);
-      //std::cout << "cycles " << Niggli::GetCycles() << std::endl;
-      std::cout << "G6 " << g << std::endl;
+
+   try {
+      CmdNiggliControls controls;
+      const BasicProgramInput<CmdNiggliControls> dc_setup("CmdNiggli", controls);
+
+      if (dc_setup.getInputList().empty()) {
+         throw std::runtime_error("; No input vectors provided");
+      }
+
+      if (controls.shouldShowControls()) {
+         std::cout << controls << std::endl;
+      }
+
+      for (const auto& input : dc_setup.getInputList()) {
+         const LRL_Cell pCell = LatticeConverter::MakePrimitiveCell(input.getLatticeType(), input.getCell());
+         G6 g;
+         const bool b = Niggli::Reduce(G6(pCell), g);
+         std::cout << "G6 " << g << std::endl;
+      }
+
+      return 0;
+   }
+   catch (const std::exception& e) {
+      std::cerr << "; An error occurred: " << e.what() << std::endl;
+      return 1;
    }
 }
