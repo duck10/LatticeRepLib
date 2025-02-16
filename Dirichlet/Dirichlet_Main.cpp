4bbde7fd828c1134e8d26cea846db00ef6d85c82
larry <larry6640995@gmail.com>
2025-02-16 15:07:31 -0800

    CmdSella, Follow, and Dirichlet -- attempt to fix web file names.

--- Dirichlet/Dirichlet_Main.cpp
+++ Dirichlet/Dirichlet_Main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
    controls.setHasWebInput(webio.m_hasWebInstructions);
    const int initblockstart = 0;
    const int initblocksize = 20;
-   const FileBlockProgramInput<DirichletControls> dc_setup("Dirichlet", initblockstart, initblocksize, controls);
+   const WebFileBlockProgramInput<DirichletControls> dc_setup(argc, argv, "Dirichlet", 0, controls);
 
    const size_t blockstart = dc_setup.getBlockStart();
    const size_t blocksize = dc_setup.getBlockSize();
